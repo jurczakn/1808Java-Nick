38a24078343fa453cc0dbf38b0c486d0fe79d6f8
@Jurczak <nickolas.jurczak@gmail.com>
2018-09-25 13:39:33 -0400

    post user

--- Week4/RedirectForward/src/main/java/com/revature/servlets/UserServlet.java
+++ Week4/RedirectForward/src/main/java/com/revature/servlets/UserServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.beans.User;
 import com.revature.services.UserService;
 
 /**
@@ -41,8 +42,12 @@ public class UserServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String userString = request.getReader().readLine();
+		System.out.println(userString);
+		ObjectMapper om = new ObjectMapper();
+		User user = om.readValue(userString, User.class);
+		System.out.println(user);
+		userService.addUser(user);
 	}
 
 	/**
