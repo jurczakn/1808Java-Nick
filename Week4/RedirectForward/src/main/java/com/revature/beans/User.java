a77f52650e766e90e3cf3ab2a01d325182b1bfd4
@Jurczak <nickolas.jurczak@gmail.com>
2018-09-25 12:02:32 -0400

    JSON Object Mapper

--- Week4/RedirectForward/src/main/java/com/revature/beans/User.java
+++ Week4/RedirectForward/src/main/java/com/revature/beans/User.java
@@ -43,6 +43,13 @@ public class User {
 		// TODO Auto-generated constructor stub
 	}
 
+	public User(String username, String password, String role) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
 	@Override
 	public String toString() {
 		return "User [username=" + username + ", password=" + password + ", role=" + role + "]";
