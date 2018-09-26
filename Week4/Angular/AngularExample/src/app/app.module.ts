9ff02ec012c8ff686aab5d224503a523b99ffd8f
@Jurczak <nickolas.jurczak@gmail.com>
2018-09-26 16:34:23 -0400

    angular

---
+++ Week4/Angular/AngularExample/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ExampleComponent } from './components/example/example.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExampleComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
