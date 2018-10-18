c01c17248fec3eae315a9deeeef98f454a3e3441
@Jurczak <nickolas.jurczak@gmail.com>
2018-10-18 16:22:51 -0400

    eureka

--- Week7/PetService/src/main/java/com/pettravel/pet/PetServiceApplication.java
+++ Week7/PetService/src/main/java/com/pettravel/pet/PetServiceApplication.java
@@ -3,8 +3,10 @@ package com.pettravel.pet;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PetServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
